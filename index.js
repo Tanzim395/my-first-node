68e1dd1a966a3a407365fb475892b36c0a5ba595
Tanzim395 <junaidulkader@gmail.com>
2021-10-21 02:19:17 +0600

    commentout

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
-const express = require('express');
-const app = express();
-const port = process.env.port || 3000;
+const express = require('express'); //express import required
+const app = express();//call the express
+const port = process.env.PORT || 3000; //set the port via environment variable or localhost 3000 or any 4 digit number as port.
 
 app.get('/', (req, res) => {
-    res.send("Hello World")
-})
+    res.send("Hello World. Welcome to the World of Node Js. Peace")
+}) //get the app, callback req & res, sometimes next then arrow function send response
 
 app.listen(port, () => {
     console.log("Listening to port", port);
-});
+}); //listen to port
+
+//run node index.js cli
