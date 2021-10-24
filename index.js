dd9a655f991441270a59986ea14183e24df31c27
Tanzim395 <junaidulkader@gmail.com>
2021-10-24 20:20:36 +0600

    sixty-three-five

--- index.js
+++ index.js
@@ -3,11 +3,47 @@ const app = express();//call the express
 const port = process.env.PORT || 3000; //set the port via environment variable or localhost 3000 or any 4 digit number as port.
 
 app.get('/', (req, res) => {
-    res.send("Hello World. Welcome to the World of Node Js. Peace")
+    res.send("Hello World. Welcome to the World of Node Js. Peace. Have A Great Day!!!")
 }) //get the app, callback req & res, sometimes next then arrow function send response
+
+const users = [
+    { id: 0, name: 'Shabna', email: 'shabana@gamil.com', phone: '016969696969' },
+    { id: 1, name: 'Detergentna', email: 'detergentna@gamil.com', phone: '016969696969' },
+    { id: 2, name: 'Handwashna', email: 'handwashna@gamil.com', phone: '016969696969' },
+    { id: 3, name: 'Vimbarna', email: 'vimberna@gamil.com', phone: '016969696969' },
+    { id: 4, name: 'Surfexcelna', email: 'surfexcelna@gamil.com', phone: '016969696969' },
+    { id: 5, name: 'Sanitizerna', email: 'sanitizerna@gamil.com', phone: '016969696969' },
+];
+
+app.get('/users', (req, res) => {
+    //use search query parameter
+    const search = req.query.search;
+    if (search) {
+        const searchResult = users.filter(user => user.name.toLocaleLowerCase().includes(search));
+        res.send(searchResult);
+    }
+    else {
+        res.send(users)
+    }
+});
+
+//dynamic api
+app.get('/users/:id', (req, res) => {
+    const id = req.params.id;
+    const user = users[id]
+    res.send(user);
+})
+
+app.get('/fruits', (req, res) => {
+    res.send(['mango', 'orange', 'lichi', 'banana'])
+})
+
+app.get('/fruits/mangoes/khirshapati', (req, res) => {
+    res.send('Yummy Tasty Mango');
+})
 
 app.listen(port, () => {
     console.log("Listening to port", port);
 }); //listen to port
 
-//run node index.js cli
+//run nodemon index.js cli
